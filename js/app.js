a694773a6cbdfee8e42e2c25592cc227d73298fa
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-09-01 20:02:11 -0300

    menu list

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
-/**
- * Scroll nav
- */
+/*                                  Scroll nav                          */
 document.addEventListener('DOMContentLoaded',()=>{
 let ubicacionPrincipal  = window.pageYOffset;
+
+setTimeout(() => {
+    document.querySelector('.hero').classList.remove('animate__animated', 'animate__fadeIn','animate__slow')
+}, 300);
+
+let UrlActual = window.location;
+let exathpath=UrlActual.pathname;
+let exathFilter=exathpath.split('/').slice();
+exathFilter.find((element=>{
+    if(element==='index.html'){
+        let separateEl=element.split('.');
+        console.log(separateEl[0]);
+        document.getElementById("index").href = "#!";
+        document.getElementById('index').addEventListener('click',()=>{
+        document.querySelector('.hero').classList.add('animationOut');
+            scroll('#start');
+        })
+    }
+    return;
+}))
+
+
 window.onscroll = function() {
     let Desplazamiento_Actual = window.pageYOffset;
     if(ubicacionPrincipal >= Desplazamiento_Actual){
@@ -21,9 +41,32 @@ window.onscroll = function() {
     ubicacionPrincipal = Desplazamiento_Actual;
 }
 
-/**
- * Rotate nav
- */
+/* internal scroll functional component*/
+
+const scroll = (component) => {
+    if(component){
+        const section = document.querySelector( component );
+        if(section){
+            section.scrollIntoView( { behavior: 'smooth', block: 'start'} );
+            const ready=document.querySelector(component);
+            ready.classList.add('animate__animated', 'animate__fadeIn','animate__slow');
+            setTimeout(() => {
+                ready.classList.remove('animate__animated', 'animate__fadeIn','animate__slower')
+            }, 3000);
+        }
+    };
+};
+
+/*                              ---------Any Scroll selector----------                           */
+
+let aboutUs=document.querySelector('#aboutUs');
+    aboutUs.addEventListener('click',()=>{
+        scroll('#mision');
+    })
+
+
+
+/*                              -----------Rotate nav-------------------                        */
 window.onload=function(){
     var cosito=document.querySelector('.navbar-toggler');
     cosito.addEventListener("click", handler);
