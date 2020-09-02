8baf31d595466271ebdd02bee03a9e98dff2e414
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-09-02 02:16:18 -0300

    finish work

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ exathFilter.find((element=>{
         })
         document.getElementById('index').classList.add('active');
     }else{
-        document.getElementById("logoFooter").href = "#!";
+        document.getElementById("index").href = "#!";
         document.getElementById('index').classList.remove('active');
     };
     if(element==='contact.html'){
